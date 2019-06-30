4e9b0e99f43cf0b228550364d34473b686e1083d
01ch01 <claudio.h1902@gmail.com7>
2019-06-30 01:09:43 -0300

    Algoritmo de Buijn quase ok. Falta fix bug duplicação do primeiro nó

--- src/bruijn.py
+++ src/bruijn.py
@@ -1,54 +1,95 @@
 class Nodo:
-    def __init__(self, prox=None, ant=None, p1=None, p2=None):
-        self.prox = prox
-        self.ant = ant
-        self.p1 = p1
-        self.p2 = p2
+    def __init__(self, item=None):
+        """
+        Nó que contém dois pares de bases nitrogenadas.
+        :param item:
+        """
+        self.item = item
 
     def __str__(self):
-        info = ''.join(self.p1)
-        info += " e " + ''.join(self.p2)
-        return info
+        return self.item
+
+    def mostrar(self):
+        return self.item
 
 
 def paired_composition(sequencia, k):
+    """
+
+    :param sequencia:
+    :param k:
+    :return:
+    """
     n_pulos = len(sequencia) - (2 * k)
 
-    no = Nodo()
+    lista_vertices = []
+    lista_arestas = []
 
     for i in range(n_pulos + 1):
-        trecho = sequencia[i:i + ((2 * k) + 1)]
+
+        # instanciando novo nodo que será adicionado logo mais
+        no = Nodo()
+
+        # primeiro par + letra de intervalo + segundo par
+        trecho = sequencia[i: i + (2 * k + 1)]
         parte1 = trecho[:k]
         parte2 = trecho[k + 1:]
 
         meio = int(k / 2)
-        if k % 2 == 1:  # caso seja ímpar
+        if k % 2 == 1:  # caso k seja ímpar, arredondar o meio pra cima
             meio += 1
 
-        no.p1 = parte1[:meio]
-        no.p2 = parte2[:meio]
+        resultado = parte1[:meio] + parte2[:meio]
+        # print(resultado)
+        no.item = resultado
 
-        print(no)
+        # inserindo nó na lista de vértices
+        lista_vertices.append(no.item)
+
+        # criando tupla de ligação do nó anterior com o nó atual
+        ligacao = (lista_vertices[i - 1], no.item)
+
+        # inserindo tupla na lista de arestas
+        lista_arestas.append(ligacao)
+
+    return [lista_vertices, lista_arestas]
 
 
-def imprimir_nos(no):
+def criar_grafo(lista_vertices, lista_arestas):
+    """
+    Cria o grafo no formato de lista de adjacência.
+    :param lista_vertices: a lista que contém os nós, as 'bolinhas' do grafo
+    :param lista_arestas: uma lista que contém várias tuplas. Cada tupla representa uma ligação de um nó com outro.
+    :return: um grafo no formato de dicionário, onde cada chave representa um elemento da lista de vértices (nó), e cada
+     valor é uma lista de tuplas representando a ligação
+    """
+    grafo = {}
+
+    for vertice in lista_vertices:
+        grafo[vertice] = []
+    for aresta in lista_arestas:
+        grafo[aresta[0]].append(aresta[1])
+
+    return grafo
+
+
+def imprimir_nodos(no):
     while no:
         print(no)
         no = no.prox
 
 
 def main():
-    # no1 = Nodo(21)
-    # no2 = Nodo(12)
-    # no3 = Nodo(23)
-    # no1.prox = no2
-    # no2.prox = no3
-    #
-    # imprimir_nos(no1)
-
     sequencia = 'TAATGCCATGGGATGTT'
     k = 3
-    paired_composition(sequencia, k)
+
+    resultado = paired_composition(sequencia, k)
+
+    lista_vertices = resultado[0]
+    lista_arestas = resultado[1]
+
+    grafo = criar_grafo(lista_vertices, lista_arestas)
+    print(grafo)
 
 
 if __name__ == '__main__':
